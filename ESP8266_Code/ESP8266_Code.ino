9e6110cfdaae42bfb5434fe04e6b1ba1138ba4fa
Ethan Bradley <97927199+Gooseboy2234@users.noreply.github.com>
2023-06-22 17:23:38 -0500

    Update ESP8266_Code.ino
    Fire retardant time!

--- ESP8266_Code/ESP8266_Code.ino
+++ ESP8266_Code/ESP8266_Code.ino
@@ -749,9 +749,9 @@ void loop() {
 
   String result = "";
   if (LED_BLINKING) digitalWrite(LED_BUILTIN, LOW);
-  // Change the part in brackets to LOW if you would like the lights to remain off and flash accordingly.
+  // Change the part in brackets to HIGH if you would like the lights to remain off and flash accordingly.
   // Rather than always on and flashing off
-  // Set line 744 to HIGH for the corresponding change to occur
+  // Set line 744 to LOW for the corresponding change to occur
   for (unsigned int duco_numeric_result = 0; duco_numeric_result < job.difficulty; duco_numeric_result++) {
     // Difficulty loop
     sha1_ctx = sha1_ctx_base;
