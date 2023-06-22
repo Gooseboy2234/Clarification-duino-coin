cfe0ee4424fecfeb18513690b59e7f05c4f70c71
Ethan Bradley <97927199+Gooseboy2234@users.noreply.github.com>
2023-06-22 17:17:46 -0500

    Update ESP32_Code.ino
    Put out the other ESP housefire, I'm aware of the other pending issue

--- ESP32_Code/ESP32_Code.ino
+++ ESP32_Code/ESP32_Code.ino
@@ -800,6 +800,9 @@ void TaskMining(void *pvParameters) {
       TaskThreadData[taskId].difficulty = jobClient.readStringUntil(MSGNEWLINE).toInt() * 100;
       jobClient.flush();
       if (LED_BLINKING) digitalWrite(LED_BUILTIN, LOW);
+      // Change the part in brackets to HIGH if you would like the lights to remain off and flash accordingly.
+      // Rather than always on and flashing off
+      // Set line 870 to LOW for the corresponding change to occur
 
       // Global Definitions
       unsigned int job_size_task_one = 100;
@@ -880,6 +883,9 @@ void TaskMining(void *pvParameters) {
           jobClient.flush();
           TaskThreadData[taskId].shares++;
           if (LED_BLINKING) digitalWrite(LED_BUILTIN, HIGH);
+          // Change the part in brackets to HIGH if you would like the lights to remain off and flash accordingly.
+          // Rather than always on and flashing off
+          // Set line 870 to LOW for the corresponding change to occur
 
           // Print statistics
           Serial.println(String(taskCoreName + " retrieved job feedback: " + feedback + ", hashrate: " + (TaskThreadData[taskId].hashrate / 1000) + "kH/s, share #" + TaskThreadData[taskId].shares));
